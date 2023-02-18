484414e44942c446ebad969a67e58942593c0e30
Emmanuel-gg <emmanuel2013161997@gmail.com>
2023-02-17 21:35:44 -0400

    General Helper, getReports for BuyerTransaction Added general Helper Added getReports for BuyerTransaction with his unit test

--- src/controller/Buyer/BuyerTransaction.js
+++ src/controller/Buyer/BuyerTransaction.js
@@ -1,4 +1,7 @@
-const { BuyerTransaction } = require('../../../database/models/index.js')
+const { Op } = require('sequelize')
+const bd = require('../../../database/models/index.js')
+const { BuyerTransaction, Buyer, sequelize } = bd
+
 const BuyerTransactionController = {
   save: async (total, tax, buyerId) => {
     const buyerEvent = new BuyerTransaction({
@@ -8,6 +11,73 @@ const BuyerTransactionController = {
     })
 
     return buyerEvent.save().then(({ id }) => ({ id }))
+  },
+  getReport: async (report) => {
+    const findAllOptions = { }
+    const attributes = []
+    const where = {
+      createdAt: {
+        [Op.gt]: report.from
+      }
+    }
+    const condition = report.condition.split(':')
+    const symbols = {
+      gt: '>',
+      lt: '<',
+      gte: '>=',
+      lte: '<=',
+      eq: '='
+    }
+    if (report.type === 'sum') {
+      // NOTE: Has to be a way to do this with sequelize
+      // PROBLEM: can have a problem of SQL injection
+      where[report.field] = sequelize.literal(`(SELECT SUM(\`${report.field}\`) FROM  \`BuyerTransaction\` AS  \`BuyerTransaction2\` WHERE \`BuyerTransaction2\`.id = \`BuyerTransaction\`.id) ${symbols[condition[0]]} ${condition[1]}`)
+      attributes.push([sequelize.fn('sum', sequelize.col(report.field)), 'sumTotal'])
+    } else {
+      where[report.field] = {
+        [Op[condition[0]]]: condition[1]
+      }
+    }
+
+    if (report.include) {
+      findAllOptions.include = report.include.map(include => {
+        const includeSplit = include.split('.')
+
+        const includeOptions = {}
+        includeOptions.model = bd[includeSplit[0].charAt(0).toUpperCase() + includeSplit[0].slice(1)]
+        includeOptions.as = includeSplit[0]
+        if (includeSplit.length) {
+          includeOptions.attributes = includeSplit[1].split(':')
+        }
+
+        return includeOptions
+      })
+    }
+    if (report.group) {
+      findAllOptions.group = report.group
+    }
+
+    findAllOptions.where = where
+    if (attributes.length) {
+      findAllOptions.attributes = attributes
+    }
+    return BuyerTransaction.findAll(
+      {
+        where,
+        attributes: [
+          [sequelize.fn('sum', sequelize.col('total')), 'sumTotal']
+        ],
+        include: [
+          {
+            model: Buyer,
+            attributes: ['id', 'name', 'lastName'],
+            as: 'buyer'
+          }
+        ],
+        group: ['buyer.id']
+
+      }
+    )
   }
 }
 module.exports = BuyerTransactionController
---
+++ src/router/BuyerTransaction/index.js
@@ -0,0 +1,30 @@
+const BuyerTransactionRouter = require('express').Router()
+
+const BuyerTransactionController = require('../../controller/Buyer/BuyerTransaction')
+const helper = require('../../helper/')
+
+BuyerTransactionRouter.get('/', (request, response, next) => {
+  let report = null
+  if (!request.query.report) {
+    return response.status(400).send({ error: 'Report is required' })
+  }
+  if (request.query.report === 'last-month-sum-total-greather-than-40') {
+    report = {
+      from: helper.getDateFromNowByMonths(-1),
+      condition: 'gt:40',
+      type: 'sum',
+      field: 'total',
+      include: ['buyer.id:name:lastName'],
+      group: 'buyer.id'
+    }
+  }
+  return BuyerTransactionController.getReport(report)
+    .then(report => {
+      response.status(200).send(report)
+    })
+    .catch(error => {
+      next(error)
+    })
+})
+
+module.exports = BuyerTransactionRouter
